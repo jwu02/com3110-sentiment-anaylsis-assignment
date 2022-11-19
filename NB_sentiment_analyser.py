ab76fcf5f84254c2b883655f6bc01adc4389c34c
jwu02 <jiacongwu31@gmail.com>
2022-11-19 22:21:50 +0000

    first working version of naive bayes model

--- NB_sentiment_analyser.py
+++ NB_sentiment_analyser.py
@@ -21,7 +21,7 @@ def parse_args():
     parser.add_argument("-dev", default="moviereviews/dev.tsv")
     parser.add_argument("-test", default="moviereviews/test.tsv")
     # parser.add_argument("-classes", type=int)
-    parser.add_argument("-classes", type=int, default=5, choices=[5, 3])
+    parser.add_argument("-classes", type=int, default=3, choices=[5, 3])
     parser.add_argument('-features', type=str, default="all_words", choices=["all_words", "features"])
     parser.add_argument('-output_files', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('-confusion_matrix', action=argparse.BooleanOptionalAction, default=False)
@@ -56,7 +56,15 @@ def main():
     Create functions and classes, using the best practices of Software Engineering
     """
 
-    def load_and_preprocess_data(filename: str) -> tuple:
+    MERGE_CLASSES_MAPPING = {
+        0: 0, # negative            -> negative
+        1: 0, # somewhat negative   -> negative
+        2: 1, # neutral             -> neutral
+        3: 2, # somewhat positive   -> positive
+        4: 2, # posititve           -> positive
+    }
+
+    def load_and_preprocess_data(filename: str, test_data=False) -> tuple:
         """
         Load data from given filename, preprocess it and return
         a tuple of a list of sentences, and a list of sentiment labels
@@ -69,31 +77,44 @@ def main():
             next(read_data, None) # skip column headings and ignore return value
             for line in read_data:
                 processed_sentence = preprocess_sentence(line[1])
-                sentiment_label = line[2]
+                
+                if number_classes == 5:
+                    sentiment_label = int(line[2])
+                else: # number_classes == 3
+                    sentiment_label = MERGE_CLASSES_MAPPING[int(line[2])]
                 
                 data.append(processed_sentence)
                 labels.append(sentiment_label)
-
+        
         return (data, labels)
 
 
     def preprocess_sentence(sentence: str) -> list:
-        sentence = sentence.split("\s")
+        sentence = sentence.split(" ")
 
         return sentence
 
 
-    def get_model() -> tuple: # training
+    def get_model() -> dict: # training
+        """
+        Return a model fitted to the training data with following keys
+        - priors: list of prior probabilities p(s_i) for all sentiment classes
+        - likelihoods: dictionary mapping sentiment class to word to its likelihood p(T|s_i)
+        - num_distinct_features: the vocabulary size of the training data
+        """
+        model = {}
+
         # load and preprocess data
         training_data, training_labels = load_and_preprocess_data(training)
-        # dev_data, dev_labels = load_and_preprocess_data(dev)
-        # different preprocess process for test data file since no sentiment labels
-        # test_data = load_and_preprocess_data(test)
 
         # list of prior probabilities p(s_i) for all sentiment classes
-        p_priors = [training_labels.count(c)/len(training_labels) for c in range(number_classes)]
-        likelihoods = {} # dict mapping sentiment class to word to its likelihood p(T|s_i)
+        priors = [training_labels.count(c)/len(training_labels) for c in range(number_classes)]
+        model['priors'] = priors
 
+        # dict mapping sentiment class to word to its likelihood p(T|s_i)
+        likelihoods = {}
+
+        # obtain word occurrences for each sentiment class first...
         for i in range(len(training_data)):
             sample = training_data[i]
             sentiment_class = training_labels[i]
@@ -106,19 +127,73 @@ def main():
                 else:
                     likelihoods[sentiment_class][w] += 1
 
+        # ... in order to calculate the likelihood
+        vocabulary = set()
         for sentiment_class in likelihoods:
-            num_features = sum(likelihoods[sentiment_class].values())
+            vocabulary.update(likelihoods[sentiment_class].keys())
+        num_distinct_features = len(vocabulary)
+        model['num_distinct_features'] = num_distinct_features
+
+        for sentiment_class in likelihoods:
+            class_occurrences_sum = sum(likelihoods[sentiment_class].values())
             for w in likelihoods[sentiment_class]:
-                # turn word counts into relative frequency/likelihood
-                likelihoods[sentiment_class][w] /= num_features
-            
-        return (p_priors, likelihoods)
+                # turn word counts into likelihood, with Laplace smoothing applied
+                likelihoods[sentiment_class][w] = (likelihoods[sentiment_class][w] + 1) / \
+                                                    (class_occurrences_sum + num_distinct_features)
+        
+        model['likelihoods'] = likelihoods
+
+        return model
 
 
-    model = get_model()
+    def evaulate_dev(data: list) -> list:
+        """
+        Given a list of preprocessed data returns a list of labels
+        the system has assigned to each data sample
+        """
+        # instantiate model represented by its parameters
+        model = get_model()
+        priors = model['priors']
+        likelihoods = model['likelihoods']
+        num_distinct_features = model['num_distinct_features']
+        
+        evaluation_labels = []
 
+        for sample in data:
+            sample_posteriors = []
+
+            for sentiment_class in range(number_classes):
+                class_occurrences_sum = sum(likelihoods[sentiment_class].values())
+                sample_likelihoods = []
+                for w in sample:
+                    if w in likelihoods[sentiment_class]:
+                        sample_likelihoods.append(likelihoods[sentiment_class][w])
+                    else:
+                        sample_likelihoods.append(1 / (class_occurrences_sum * num_distinct_features))
+                
+                # calculate product of all word likelihoods
+                likelihood = 1
+                for x in sample_likelihoods:
+                    likelihood *= x
+
+                # calculate and record posterior
+                sample_posteriors.append(priors[sentiment_class] * likelihood)
+
+            # assign class label as index of the maxmimum posterior
+            evaluation_labels.append(sample_posteriors.index(max(sample_posteriors)))
+
+        return evaluation_labels
+
+
+    dev_data, dev_labels = load_and_preprocess_data(dev)
+    evaluated_labels = evaulate_dev(dev_data)
+
+    correct = 0
+    for i in range(len(evaluated_labels)):
+        if evaluated_labels[i] == dev_labels[i]:
+            correct += 1
     
-
+    print(f"Score: {correct} out of {len(evaluated_labels)} correct")
 
     
     #You need to change this in order to return your macro-F1 score for the dev set
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## TODO
 - preprocessing
     - lowercasing
-- implement naive bayes from scratch
-- implement function to have only 3 sentiment values positive, negative, neutral
+- implement naive bayes from scratch (evaluation left)
+- implement function to have only 3 sentiment values positive, negative, neutral (done)
 - train 4 models
     - considering all words, with 5 and 3 value sentiment score
     - considering chosen features, with 5 and 3 value sentiment score
